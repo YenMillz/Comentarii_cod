8c4b18f1b242c2405f81336ffb644c62534b9040
YenMillz <68549495+YenMillz@users.noreply.github.com>
2021-10-21 00:09:19 +0300

    main
    main_code

---
+++ Main.java
@@ -0,0 +1,19 @@
+// days until deadline
+int d;
+// if the student is at least 18 years of age
+if (student.Age> = 18)
+{
+    // send meeting invitation to the student
+    notificationService.SendMessageTo(student, meetingInvitation);
+}
+// if the student is younger than 18 years
+else 
+{
+    // sends a meeting invitation to the student's legal guardian
+    notificationService.SendMessageTo(student.Parent, meetingInvitation);
+}
+// student is eligible for blood donation
+if (student.Age >= 17 && student.Weight >= 58.0 && student.Height >= 1.55)
+{
+    ScheduleBloodDonatingSessionWith(student);
+}
